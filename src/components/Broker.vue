40054f5466787ecdb52a2cb18dd23e40228e2b9e
Daniel Lando <daniel.sorridi@gmail.com>
2019-06-12 16:53:23 +0200

    [style] Complete restayled broker UI and added clients status

--- src/components/Broker.vue
+++ src/components/Broker.vue
@@ -1,16 +1,18 @@
 /* eslint-disable */
 
 <template>
-  <v-container fluid fill-height>
+  <v-container fluid fill-height grid-list-md>
     <v-layout align-center justify-center>
       <v-flex xs12 sm8 md4>
         <v-card class="elevation-12">
           <v-toolbar dark color="primary">
-            <v-toolbar-title>MQTT Broker</v-toolbar-title>
+            <v-toolbar-title>Status</v-toolbar-title>
             <v-spacer></v-spacer>
           </v-toolbar>
           <v-card-text>
             <v-list dense>
+              <v-subheader>General</v-subheader>
+              <v-divider></v-divider>
               <v-list-tile>
                 <v-list-tile-content>Broker</v-list-tile-content>
                 <v-list-tile-content
@@ -36,7 +38,11 @@
                 <v-list-tile-content>Key</v-list-tile-content>
                 <v-list-tile-content class="align-end">
                   <v-tooltip bottom>
-                      <v-icon slot="activator" color="primary" @click.stop="download(status.key, 'key.pem')">get_app</v-icon>
+                    <v-icon
+                      slot="activator"
+                      color="primary"
+                      @click.stop="download(status.key, 'key.pem')"
+                    >get_app</v-icon>
                     <span>Download</span>
                   </v-tooltip>
                 </v-list-tile-content>
@@ -45,203 +51,81 @@
                 <v-list-tile-content>Cert</v-list-tile-content>
                 <v-list-tile-content class="align-end">
                   <v-tooltip bottom>
-                      <v-icon slot="activator" color="primary" @click.stop="download(status.cert, 'cert.pem')">get_app</v-icon>
+                    <v-icon
+                      slot="activator"
+                      color="primary"
+                      @click.stop="download(status.cert, 'cert.pem')"
+                    >get_app</v-icon>
                     <span>Download</span>
                   </v-tooltip>
                 </v-list-tile-content>
               </v-list-tile>
+              <div v-if="status.clients && status.clients.length > 0">
+                <v-subheader>Clients</v-subheader>
+                <v-divider></v-divider>
+                <v-list-tile v-for="(client, index) in status.clients" :key="index">
+                  <v-list-tile-content>{{client.config.name}}</v-list-tile-content>
+                  <v-list-tile-content
+                    :style="{color: (client.status ? 'green' : 'red')}"
+                    class="align-end"
+                  >{{client.status ? 'Connected' : client.error}}</v-list-tile-content>
+                </v-list-tile>
+              </div>
             </v-list>
-            <v-divider></v-divider>
-            <v-expansion-panel class="elevation-0">
-              <v-expansion-panel-content>
-                <div slot="header">Settings</div>
-                <v-form id="form_broker" v-model="valid" ref="form_broker">
-                  <v-text-field
-                    v-model.number="edited.port"
-                    :disabled="disabled"
-                    prepend-icon="settings_ethernet"
-                    label="Port"
-                    :rules="[rules.required, validPort]"
-                    hint="Broker listening port"
-                    required
-                    type="number"
-                  ></v-text-field>
-                  <v-switch
-                    :disabled="disabled"
-                    persistent-hint
-                    label="MQTT over Websockets"
-                    v-model="edited.websocket"
-                  ></v-switch>
-                  <v-text-field
-                    v-if="edited.websocket"
-                    :disabled="disabled"
-                    prepend-icon="settings_ethernet"
-                    v-model.number="edited.webPort"
-                    label="Websocket port"
-                    :rules="[rules.required]"
-                    hint="Websocket listening port"
-                    required
-                    type="number"
-                  ></v-text-field>
-                  <v-switch
-                    :disabled="disabled"
-                    persistent-hint
-                    label="Require auth"
-                    v-model="edited.authenticate"
-                  ></v-switch>
-                  <v-text-field
-                    v-if="edited.authenticate"
-                    :disabled="disabled"
-                    prepend-icon="person"
-                    v-model.number="edited.username"
-                    label="Username"
-                    :rules="[requiredAuth]"
-                    required
-                  ></v-text-field>
-                  <v-text-field
-                    v-if="edited.authenticate"
-                    :disabled="disabled"
-                    prepend-icon="lock"
-                    v-model.number="edited.password"
-                    label="Password"
-                    :rules="[requiredAuth]"
-                    required
-                    type="password"
-                  ></v-text-field>
-                  <v-switch :disabled="disabled" persistent-hint label="SSL" v-model="edited.ssl"></v-switch>
-                  <v-switch
-                    persistent-hint
-                    v-if="edited.ssl"
-                    :disabled="disabled"
-                    label="Auto-generate ssl certs"
-                    v-model="edited.autoGenerate"
-                    @change="changeAutogen"
-                  ></v-switch>
-                  <v-switch
-                    persistent-hint
-                    :disabled="disabled"
-                    v-if="edited.ssl"
-                    label="Allow Self-signed certs"
-                    v-model="edited.selfSigned"
-                  ></v-switch>
-                  <file-input
-                    :rules="[requiredKey]"
-                    :disabled="disabled"
-                    v-if="edited.ssl && !edited.autoGenerate"
-                    label="Key.pem"
-                    v-model="edited.key"
-                    @onFileSelect="onFileKeySelect"
-                  ></file-input>
-                  <file-input
-                    :disabled="disabled"
-                    :rules="[requiredCert]"
-                    v-if="edited.ssl && !edited.autoGenerate"
-                    label="Cert.pem"
-                    v-model="edited.cert"
-                    @onFileSelect="onFileCertSelect"
-                  ></file-input>
-                  <v-btn
-                    color="blue darken-1"
-                    flat
-                    @click="disabled ? disabled = false : validateSettings()"
-                  >{{disabled ? 'Edit' : 'Update'}}</v-btn>
-                  <v-btn v-if="!disabled" color="blue darken-1" flat @click="undoEdits()">Cancel</v-btn>
-                </v-form>
-              </v-expansion-panel-content>
-            </v-expansion-panel>
           </v-card-text>
         </v-card>
       </v-flex>
     </v-layout>
+
+    <!--FAB-->
+    <v-btn fab bottom right dark fixed color="blue" @click.stop="openDialog">
+        <v-icon>settings</v-icon>
+    </v-btn>
+
+    <DialogSettings
+      @save="saveValue"
+      @close="closeDialog"
+      v-model="dialogValue"
+      :editedValue="editedValue"
+    />
   </v-container>
 </template>
 
 <script>
 import { mapGetters, mapMutations } from "vuex";
-import fileInput from "@/components/custom/file-input.vue";
-import { readFile } from "fs";
 import ConfigApis from "@/apis/ConfigApis";
-import { setInterval, clearInterval } from "timers";
+import DialogSettings from "@/components/dialogs/Settings";
+
 
 export default {
   name: "Broker",
-  components: { fileInput },
+  components: { DialogSettings },
   computed: {
-    ...mapGetters(["broker"]),
-    validPort() {
-      return (
-        (this.edited.websocket
-          ? this.edited.port != this.edited.webPort
-          : true) || "Port and websocket port must be differents"
-      );
-    },
-    requiredAuth() {
-      return (
-        !this.edited.authenticate ||
-        (!!this.edited.username && !!this.edited.password) ||
-        "Insert valid username and password"
-      );
-    },
-    requiredKey() {
-      return (
-        (this.edited.ssl && !this.edited.autoGenerate && !!this.edited.key) ||
-        "You must select a File"
-      );
-    },
-    requiredCert() {
-      return (
-        (this.edited.ssl && !this.edited.autoGenerate && !!this.edited.cert) ||
-        "You must select a File"
-      );
-    }
-  },
-  watch: {
-    broker: function(newValue) {
-      this.edited = Object.assign({}, newValue);
-    }
+    ...mapGetters(["broker"])
   },
   data() {
     return {
-      valid: true,
       status: {},
-      edited: {},
-      disabled: true,
-      statusInterval: null,
-      rules: {
-        required: value => {
-          var valid = false;
-
-          if (value instanceof Array) valid = value.length > 0;
-          else valid = !!value || value === 0;
-
-          return valid || "This field is required.";
-        }
-      }
+      dialogValue: false,
+      editedValue: {},
+      defaultValue: {},
+      statusInterval: null
     };
   },
   methods: {
+    openDialog () {
+      this.editedValue = Object.assign({}, this.broker);
+      this.dialogValue = true
+    },
+    closeDialog () {
+      this.dialogValue = false
+    },
+    saveValue () {
+      this.$store.dispatch("updateBroker", this.editedValue);
+      this.closeDialog()
+    },
     showSnackbar(text) {
       this.$emit("showSnackbar", text);
-    },
-    undoEdits() {
-      this.edited = Object.assign({}, this.broker);
-      this.disabled = true;
-    },
-    validateSettings() {
-      if (this.$refs.form_broker.validate()) {
-        this.$store.dispatch("updateBroker", this.edited);
-        this.disabled = true;
-        this.showSnackbar("Press save to update settings on server");
-      } else {
-        this.showSnackbar("Broker settings not valid");
-      }
-    },
-    changeAutogen() {
-      var edited = this.edited;
-      if (edited.autoGenerate) {
-        edited._key = edited.key = "";
-        edited._cert = edited.cert = "";
-      }
     },
     download(text, filename) {
       var element = document.createElement("a");
@@ -258,26 +142,6 @@ export default {
 
       document.body.removeChild(element);
     },
-    readFile(file, callback) {
-      const reader = new FileReader();
-
-      reader.onload = e => callback(e.target.result);
-      reader.readAsText(file);
-    },
-    onFileKeySelect(files) {
-      var file = files[0];
-      var self = this;
-      if (file) {
-        this.readFile(file, text => (self.edited._key = text));
-      }
-    },
-    onFileCertSelect(files) {
-      var file = files[0];
-      var self = this;
-      if (file) {
-        this.readFile(file, text => (self.edited._cert = text));
-      }
-    },
     async updateStatus() {
       try {
         var result = await ConfigApis.getStatus();
@@ -290,7 +154,6 @@ export default {
   },
   async mounted() {
     await this.updateStatus();
-    this.edited = Object.assign({}, this.broker);
     this.statusInterval = setInterval(this.updateStatus.bind(this), 2000);
   },
   beforeDestroy() {
---
+++ src/components/dialogs/Settings.vue
@@ -0,0 +1,229 @@
+<template>
+  <v-dialog v-model="value" max-width="500px" persistent>
+    <v-card>
+      <v-card-title>
+        <span class="headline">Broker settings</span>
+      </v-card-title>
+
+      <v-card-text>
+        <v-container grid-list-md>
+          <v-form v-model="valid" ref="form" lazy-validation>
+            <v-layout wrap>
+              <v-flex xs12>
+                <v-text-field
+                  v-model.number="editedValue.port"
+                  label="Port"
+                  :rules="[rules.required, validPort]"
+                  hint="Broker listening port"
+                  required
+                  type="number"
+                ></v-text-field>
+              </v-flex>
+              <v-flex xs12 sm6>
+                <v-switch
+                  persistent-hint
+                  label="Websocket"
+                  hint="Enable this for MQTT over websockets"
+                  v-model="editedValue.websocket"
+                ></v-switch>
+              </v-flex>
+              <v-flex xs12 sm6>
+                <v-text-field
+                  v-if="editedValue.websocket"
+                  v-model.number="editedValue.webPort"
+                  label="Websocket Port"
+                  :rules="[rules.required]"
+                  hint="Websocket listening port"
+                  required
+                  type="number"
+                ></v-text-field>
+              </v-flex>
+              <v-flex xs12>
+                <v-switch
+                  persistent-hint
+                  label="Require auth"
+                  v-model="editedValue.authenticate"
+                  hint="Set a custom username and a password for clients auth"
+                ></v-switch>
+              </v-flex>
+              <v-flex xs12 sm6>
+                <v-text-field
+                  v-if="editedValue.authenticate"
+                  v-model.number="editedValue.username"
+                  label="Username"
+                  :rules="[requiredAuth]"
+                  required
+                ></v-text-field>
+              </v-flex>
+              <v-flex xs12 sm6>
+                <v-text-field
+                  v-if="editedValue.authenticate"
+                  v-model.number="editedValue.password"
+                  label="Password"
+                  :append-icon="e1 ? 'visibility' : 'visibility_off'"
+                  @click:append="() => (e1 = !e1)"
+                  :type="e1 ? 'password' : 'text'"
+                  :rules="[requiredAuth]"
+                  required
+                ></v-text-field>
+              </v-flex>
+              <v-flex xs12 sm6>
+                <v-switch
+                  persistent-hint
+                  label="SSL"
+                  v-model="editedValue.ssl"
+                  hint="Enable this for encrypted cominications"
+                ></v-switch>
+              </v-flex>
+              <v-flex xs12 sm6>
+                <v-switch
+                  persistent-hint
+                  v-if="editedValue.ssl"
+                  hint="Enable this to allow self-signed certs"
+                  label="Self-signed certs"
+                  v-model="editedValue.selfSigned"
+                ></v-switch>
+              </v-flex>
+              <v-flex xs12>
+                <v-switch
+                  persistent-hint
+                  v-if="editedValue.ssl"
+                  label="Auto-generate certs"
+                  hint="Enable this to auto-generate ssl certs using node-pem"
+                  v-model="editedValue.autoGenerate"
+                  @change="changeAutogen"
+                ></v-switch>
+              </v-flex>
+              <v-flex xs12 sm6>
+                <file-input
+                  :rules="[requiredKey]"
+                  v-if="editedValue.ssl && !editedValue.autoGenerate"
+                  label="Key.pem"
+                  v-model="editedValue.key"
+                  @onFileSelect="onFileKeySelect"
+                ></file-input>
+              </v-flex>
+              <v-flex xs12 sm6>
+                <file-input
+                  :rules="[requiredCert]"
+                  v-if="editedValue.ssl && !editedValue.autoGenerate"
+                  label="Cert.pem"
+                  v-model="editedValue.cert"
+                  @onFileSelect="onFileCertSelect"
+                ></file-input>
+              </v-flex>
+            </v-layout>
+          </v-form>
+        </v-container>
+      </v-card-text>
+
+      <v-card-actions>
+        <v-spacer></v-spacer>
+        <v-btn color="blue darken-1" flat @click="$emit('close')">Cancel</v-btn>
+        <v-btn color="blue darken-1" flat @click="$refs.form.validate() && $emit('save')">Save</v-btn>
+      </v-card-actions>
+    </v-card>
+  </v-dialog>
+</template>
+
+<script>
+import { mapGetters } from "vuex";
+import fileInput from "@/components/custom/file-input.vue";
+
+export default {
+  props: {
+    value: Boolean,
+    title: String,
+    editedValue: Object
+  },
+  components: { fileInput },
+  watch: {
+    value(val) {
+      this.$refs.form.resetValidation();
+    }
+  },
+  computed: {
+    validPort() {
+      return (
+        (this.editedValue.websocket
+          ? this.editedValue.port != this.editedValue.webPort
+          : true) || "Port and websocket port must be differents"
+      );
+    },
+    requiredAuth() {
+      return (
+        !this.editedValue.authenticate ||
+        (!!this.editedValue.username && !!this.editedValue.password) ||
+        "Insert valid username and password"
+      );
+    },
+    requiredKey() {
+      return (
+        (this.editedValue.ssl &&
+          !this.editedValue.autoGenerate &&
+          !!this.editedValue.key) ||
+        "You must select a File"
+      );
+    },
+    requiredCert() {
+      return (
+        (this.editedValue.ssl &&
+          !this.editedValue.autoGenerate &&
+          !!this.editedValue.cert) ||
+        "You must select a File"
+      );
+    }
+  },
+  methods: {
+    changeAutogen() {
+      var edited = this.editedValue;
+      if (edited.autoGenerate) {
+        edited._key = edited.key = "";
+        edited._cert = edited.cert = "";
+      }
+    },
+    readFile(file, callback) {
+      const reader = new FileReader();
+
+      reader.onload = e => callback(e.target.result);
+      reader.readAsText(file);
+    },
+    onFileKeySelect(files) {
+      var file = files[0];
+      var self = this;
+      if (file) {
+        this.readFile(file, text => (self.editedValue._key = text));
+      }
+    },
+    onFileCertSelect(files) {
+      var file = files[0];
+      var self = this;
+      if (file) {
+        this.readFile(file, text => (self.editedValue._cert = text));
+      }
+    }
+  },
+  data() {
+    return {
+      valid: true,
+      e1: false,
+      rules: {
+        required: value => {
+          var valid = false;
+
+          if (value instanceof Array) valid = value.length > 0;
+          else valid = !!value || value === 0;
+
+          return valid || "This field is required.";
+        },
+        validName: value => {
+          return (
+            !/[!@#$%^&*)(+=:,;"'\\|?{}£°§<>[\]/.\s]/g.test(value) ||
+            'Name is not valid, only "a-z" "A-Z" "0-9" chars and "_" are allowed'
+          );
+        }
+      }
+    };
+  }
+};
+</script>
