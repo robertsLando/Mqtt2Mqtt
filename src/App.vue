bbac3f56ab973980164cdc616eba4a620fe294b3
Daniel Lando <daniel.sorridi@gmail.com>
2019-09-26 09:58:05 +0200

    [fix] Drawer mini on init

--- src/App.vue
+++ src/App.vue
@@ -190,7 +190,7 @@ export default {
       drawer: false,
       topbar: [],
       title: "",
-      mini: true,
+      mini: false,
       snackbar: false,
       snackbarText: "",
       items: [
